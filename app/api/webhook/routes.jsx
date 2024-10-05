5fab7ad4a454a27b14f3ce1e0938dac73b891e07
gaurav5xy <gauravkumar81464@gmail.com>
2024-10-05 15:58:44 +0530

    rename env

--- app/api/webhook/routes.jsx
+++ app/api/webhook/routes.jsx
@@ -6,7 +6,7 @@ const { NextResponse } = require('next/server');
 
 module.exports.POST = async function(req) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local');
