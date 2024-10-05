8da54ee30c0a90d01a75369e36c160f0798ace5f
gaurav5xy <gauravkumar81464@gmail.com>
2024-10-05 16:19:15 +0530

    webHook

--- app/page.js
+++ app/page.js
@@ -4,15 +4,15 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-  <>
-    <main className="bg-[#212121] text-white min-h-screen flex flex-col justify-center items-center">
-      <div className=" self-start ">
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      </div>
+    <>
+      <main className="bg-[#212121] text-white min-h-screen flex flex-col justify-center items-center">
+        <div className=" self-start ">
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
         <UrlInput />
-    </main>
-  </>
+      </main>
+    </>
   );
 }
