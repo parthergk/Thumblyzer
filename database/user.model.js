fa671714f81e6e2341c53a61b4e701bfce3e6251
gaurav5xy <gauravkumar81464@gmail.com>
2024-10-07 12:06:20 +0530

    ts

--- database/user.model.js
+++ database/user.model.js
@@ -1,12 +1,10 @@
 import { Schema, model, models } from "mongoose";
 
 const userSchema = new Schema({
-    clerkId: {type: String, require: true},
-    usename: {type:String, require: true, unique : true},
-    email: {type: String, require: true, unique : true},
+    clerkId: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
 });
 
-// Avoid model recreation if already exists
 const User = models.User || model('User', userSchema);
-
 export default User;
